47736548b575560ab5bd35934817bccf83808984
Kevin Colley <kjcolley7@gmail.com>
2017-03-24 02:17:32 -0400

    Simplified factorial test case

--- test_cases/input015.pas
+++ test_cases/input015.pas
@@ -2,12 +2,10 @@ var n, fact;
 
 procedure Factorial(n);
 begin
-	if n > 0 then begin
-		return := n * call Factorial(n - 1);
-	end
-	else begin
-		return := 1;
-	end;
+	if n > 1 then
+		return := n * call Factorial(n - 1)
+	else
+		return := 1
 end;
 
 begin
